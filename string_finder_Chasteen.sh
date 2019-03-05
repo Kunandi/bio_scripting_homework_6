7c489a68f9661ff1cf4d802f8c7faab594431579
Kunnadi <theliteraryagrarian@gmail.com>
2019-03-05 15:40:12 -0600

    removing extrainious file

--- string_finder_Chasteen.sh
+++
@@ -1,9 +0,0 @@
-#In this exercise you will write a script that searches for a specific word (i.e., string) in a file and displays how many times and on what line numbers of the file they were found.
-#3) Hint: review how the -n flag changes the output of grep. How could you use this together with cut to find all line numbers where the specific word (i.e., string) was found?
-#4) Proceed to write the script, taking a filename as the first argument and the word (i.e., string) being searched for as a second argument.
-#5) The script should display a summary message, showing a) how many matches were found at least once per line, b) the line numbers where the string occurred at least once 
-#per line and c) the file name being searched.
-#6) Make a copy of the directory book_pages and its contents from the directory biobootcamp/scripting_4_biologists/homework_files on the ASC
-# and place it in your “part_2” directory. Run your script against the 22 *.txt files located in this directory via embedding your script from above
-# into a 2nd script comprising a for-do- done loop and redirect the output from standard output to a file. You may delete the directory book_pages when you are done with it.
-#7) Stage and commit the “string_finder_YOURLASTNAME.sh” and for-do-done scripts along with the output file with git.
